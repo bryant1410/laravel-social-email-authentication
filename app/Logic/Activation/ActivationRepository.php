68885f0fba6a47bd81db44251625d6b421b74aaa
Ivan Radunovic <ivan@codingo.me>
2016-11-18 13:56:02 +0100

    Limiting number of activation attempts.

--- app/Logic/Activation/ActivationRepository.php
+++ app/Logic/Activation/ActivationRepository.php
@@ -12,6 +12,14 @@ class ActivationRepository
 
     public function createTokenAndSendEmail(User $user)
     {
+        // Limit number of activation attempts to 3 in 24 hours window
+        $activations = Activation::where('user_id', $user->id)
+            ->where('created_at', '>=', Carbon::now()->subHours(24))
+            ->count();
+
+        if ($activations >= 3) {
+            return true;
+        }
 
         if ($user->activated) { //if user changed activated email to new one
 
